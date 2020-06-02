57582a48bb1709777ed9805165d40b30d8790105
Andreas Jaeger <aj@suse.com>
2020-06-02 14:18:56 +0200

    Switch to newer openstackdocstheme version
    Switch to openstackdocstheme 2.2.1 version. Using
    this version will allow especially:
    * Linking from HTML to PDF document
    * Allow parallel building of documents
    * Fix some rendering problems
    
    Update Sphinx version as well.
    
    Disable openstackdocs_auto_name to use 'project' variable as name.
    
    Change pygments_style to 'native' since old theme version always used
    'native' and the theme now respects the setting and using 'sphinx' can
    lead to some strange rendering.
    
    openstackdocstheme renames some variables, so follow the renames
    before the next release removes them. A couple of variables are also
    not needed anymore, remove them.
    
    See also
    http://lists.openstack.org/pipermail/openstack-discuss/2020-May/014971.html
    
    Change-Id: I6e9989f1882cf5729e644e240311c9b73bd578b6

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -26,9 +26,10 @@ extensions = [
 ]
 
 # openstackdocstheme options
-repository_name = 'openstack/os-performance-tools'
-bug_project = 'os-performance-tools'
-bug_tag = ''
+openstackdocs_repo_name = 'openstack/os-performance-tools'
+openstackdocs_auto_name = False
+openstackdocs_bug_project = 'os-performance-tools'
+openstackdocs_bug_tag = ''
 
 html_theme = 'openstackdocs'
 
@@ -62,7 +63,7 @@ add_function_parentheses = True
 add_module_names = True
 
 # The name of the Pygments (syntax highlighting) style to use.
-pygments_style = 'sphinx'
+pygments_style = 'native'
 
 
 # -- Options for HTML output --------------------------------------------------
